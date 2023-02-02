5759371f31885d2626f759e2201a5b6dc8053081
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-02-02 17:56:16 +0530

    ggs

---
+++ Frontend/src/app/listing/page.tsx
@@ -0,0 +1,15 @@
+import List from 'Components/List/List'
+import React from 'react'
+
+type Props = {}
+
+const Listing = (props: Props) => {
+  return (
+    <div className='flex min-h-screen'>
+        
+        <List/>
+    </div>
+  )
+}
+
+export default Listing
