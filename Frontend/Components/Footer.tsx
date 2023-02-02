d662185e6590cd242b7294b39f8c8f9c8c532039
Mrunmayi <mrunmayirparker@gmail.com>
2023-02-02 19:39:44 +0530

    navbar, footer, model,card  modify

--- Frontend/Components/Footer.tsx
+++ Frontend/Components/Footer.tsx
@@ -1,32 +1,28 @@
 import Link from "next/link";
 import React from "react";
-import { FaFacebook } from "react-icons/fa";
-import { BsLinkedin, BsTwitter, BsGithub } from "react-icons/bs";
+// import { FaFacebook } from "react-icons/fa";
+import { BsLinkedin,BsInstagram , BsTwitter, BsGithub } from "react-icons/bs";
 type Props = {}
 
 const Footer = (props: Props) => {
+
+ 
   return (
     <div className=" bg-gray-100 dark:bg-gray-900 ">
-   <div className="max-w-screen-xl  px-4 sm:px-6 text-gray-800 sm:flex justify-between mx-auto">
-      <div className="p-5 sm:w-8/12">
-        <h3 className="font-bold text-3xl text-indigo-600 mb-4">Lesss Gooo</h3>
-        
-        <div className="flex text-gray-500 uppercase text-sm">
-          <a href="#" className="mr-2 hover:text-indigo-600">Home</a>
-          <a href="#" className="mr-2 hover:text-indigo-600">Blog</a>
-          <a href="#" className="mr-2 hover:text-indigo-600">Projects</a>
-          <a href="#" className="mr-2 hover:text-indigo-600">About</a>
+   {/* <div className="max-w-screen-xl  px-4 sm:px-6 text-gray-800 sm:flex justify-between mx-auto">
+      <div className="p-5 sm:w-8/12"> */}
+        <h3 className="font-bold text-6xl text-[#25495c] text-center pt-20">DigiBart</h3>   
+        <p className="m-10 px-20 text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis fugit temporibus fuga voluptate, tenetur mollitia eligendi voluptatem ducimus! Ipsam, consequatur dolor reprehenderit alias ullam excepturi pariatur tempore odio odit repellendus.</p>
+        <div className="flex flex-row space-x-6 justify-center text-2xl pb-10 cursor-pointer">
+         <div > <BsInstagram  /></div>
+         <div><BsLinkedin /></div>
+         <div><BsTwitter /></div>
+         <div><BsGithub /></div>
         </div>
-       </div>
-     <div className="p-5 sm:w-4/12">
-       <h3 className="font-medium text-lg text-indigo-600 mb-4">Subscribe to Newsletter</h3>
-       <form className="mt-4">
-      <input className="border rounded w-full px-4 py-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="username@email.com"/>
-      </form>
-     </div>
+        {/* <div className="flex text-gray-500 uppercase text-sm"></div> */}
    </div>
- 
-</div>
+//        </div>
+// </div>
  
   )
 }
