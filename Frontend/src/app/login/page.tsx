0af4d6039a56856c7d1a909ffb3d05d0992ae0e7
Jayu Jain <jayujaiprakashjain@gmail.com>
2023-02-02 17:54:15 +0530

    login , register , card form modified

--- Frontend/src/app/login/page.tsx
+++ Frontend/src/app/login/page.tsx
@@ -2,11 +2,13 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import {motion } from "framer-motion"
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { FaGithub, FaGoogle, FaInstagram, FaFacebook  } from "react-icons/fa";
 import Link from "next/link";
 type Props = {};
 import {useSession, signIn, signOut} from 'next-auth/react'
 import { useRouter } from 'next/navigation';
+
+
 
 const Login = (props: Props) => {
   const [email, setEmail] = useState("");
@@ -49,14 +51,18 @@ const Login = (props: Props) => {
 
   const [count, setCount] = React.useState(0);  
   return (
-    <section className="bg-purple-900 dark:bg-black ">
+    <section className="bg-[#25495c] dark:bg-black ">
       <div className="px-0 py-1 mx-auto max-w-7xl sm:px-4">
         <motion.div   key={count}
       animate={{ scale: 0.9 }}
       transition={{ duration: 1 }}
       
-   className="w-full px-4 pt-5 pb-6 mx-auto mt-8 mb-6 bg-white rounded-none shadow-xl sm:rounded-lg sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6">
-          <h1 className="mb-4 text-lg font-semibold text-left text-gray-900">
+          className="w-full px-4 pt-5 pb-6 mx-auto mt-8 mb-6 bg-white rounded-none shadow-xl sm:rounded-lg sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6">
+           <div className="flex mx-8 justify-evenly text-3xl pb-4 border-b-2" >
+            <FaInstagram  />
+            <FaFacebook />
+    </div>
+          <h1 className="mb-4 pt-4 text-lg font-semibold text-left text-[#25495c]">
             Log in to your account
           </h1>
           <form className="mb-8 space-y-4" onSubmit={sbmitHandler}>
@@ -92,7 +98,7 @@ const Login = (props: Props) => {
             </label>
             <input
               type="submit"
-              className="bg-blue-200 w-full py-3 mt-1 justify-center btn btn-primary"
+              className="bg-[#e2edf2] text-[#25495c] hover:bg-[#25495c] hover:text-[#e2edf2] hover:cursor-pointer w-full py-3 mt-1 justify-center btn btn-primary"
               value="Login"
             />
           </form>
@@ -102,26 +108,26 @@ const Login = (props: Props) => {
                 Or
               </span>
             </div>
-            <div className="grid grid-cols-2 gap-4">
-              <button onClick={handleGoogleSignIn} className="py-3 btn btn-icon btn-google ">
+            <div className="grid grid-cols-2  gap-4 justify-center">
+              <button onClick={handleGoogleSignIn} className="py-3 btn bg-[#e2edf2] text-[#25495c] hover:bg-[#25495c] hover:text-[#e2edf2] hover:cursor-pointer btn-icon btn-google ">
                <FaGoogle className="mr-1 text-2xl"/>
-                <span className="sr-only">Continue with</span> Google
+                <span className="sr-only ">Continue with</span> Google
               </button>
-              <a href="#" className="py-3 btn btn-icon btn-dark">
+              {/* <a href="#" className="py-3 btn btn-icon bg-[#e2edf2] text-[#25495c] hover:bg-[#25495c] hover:text-[#e2edf2] hover:cursor-pointer btn-dark">
                 <FaGithub className="mr-1 text-2xl"/>
                 <span className="sr-only">Continue with</span> Github
-              </a>
+              </a> */}
             </div>
           </div>
         </motion.div>
         <p className="mb-4  text-xs text-center text-gray-400">
-          <Link href="/register" className="text-purple-200 underline hover:text-white">
+          <Link href="/register" className="text-[#e2edf2]  underline hover:text-white">
             Create an account
           </Link>
           ·
           
           ·
-          <a href="#" className="text-purple-200 underline hover:text-white">
+          <a href="#" className="text-[#e2edf2] underline ">
             Privacy & Terms
           </a>
         </p>
--- Frontend/src/app/register/page.tsx
+++ Frontend/src/app/register/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import {motion } from "framer-motion"
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { FaGithub, FaGoogle, FaInstagram, FaFacebook } from "react-icons/fa";
 import Link from "next/link";
 import axios from "axios";
 import { useSession } from "next-auth/react";
@@ -13,8 +13,9 @@ const Register = (props: Props) => {
   const [count, setCount] = React.useState(0);  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const[name, setName] = useState("");
+  // const [password2, setPassword2] = useState("");
+  const [name, setName] = useState("");
+  const[city,setCity] = useState("")
 
   const submitHandler = async (e:any) => {
     e.preventDefault();
@@ -41,14 +42,22 @@ const Register = (props: Props) => {
 
 
   return (
-    <section className="bg-purple-900 dark:bg-black ">
+    <section className="bg-[#25495c] dark:bg-black ">
+
+      
       <div className="px-0 mx-auto py-1 max-w-7xl sm:px-4">
         <motion.div   key={count}
       animate={{ scale: 0.9 }}
       transition={{ duration: 1 }}
-      
-   className="w-full px-4 pt-5 pb-6 mx-auto mt-8 mb-6 bg-white rounded-none shadow-xl sm:rounded-lg sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6">
-          <h1 className="mb-4 text-lg font-semibold text-left text-gray-900">
+    
+   
+          className="w-full px-4 pt-5 pb-6 mx-auto mt-8 mb-6 bg-white rounded-none shadow-xl sm:rounded-lg sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6">
+             <div className="flex justify-evenly text-3xl pb-4 border-b-2" >
+            <FaInstagram  />
+            
+      <FaFacebook />
+    </div>
+          <h1 className="mb-4 pt-4 text-lg font-semibold text-left text-[#25495c]">
             Log in to your account
           </h1>
           <form className="mb-8 space-y-4" onSubmit={submitHandler}>
@@ -60,6 +69,21 @@ const Register = (props: Props) => {
                 className="form-input"
                 type="text"
                 placeholder="Ex. FirstName LastName"
+                required
+                onChange={(e) => {
+                  setName(e.target.value);
+                }
+                }
+              />
+            </label>
+            <label className="block">
+              <span className="block mb-1 text-xs font-medium text-gray-700">
+                Phone No.
+              </span>
+              <input
+                className="form-input"
+                type="text"
+                placeholder="Ex. 9784564512"
                 required
                 onChange={(e) => {
                   setName(e.target.value);
@@ -99,18 +123,22 @@ const Register = (props: Props) => {
 
             <label className="block">
               <span className="block mb-1 text-xs font-medium text-gray-700">
-                Confirm Password
+                City
               </span>
               <input
-                className="form-input "
-                type="password2"
-                placeholder="••••••••"
+                className="form-input"
+                type="text"
+                placeholder="Enter you city"
                 required
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }
+                }
               />
             </label>
             <input
               type="submit"
-              className="bg-blue-200 w-full py-3 mt-1 justify-center btn btn-primary"
+              className="bg-[#e2edf2] text-[#25495c] hover:bg-[#25495c] hover:text-[#e2edf2] hover:cursor-pointer w-full py-3 mt-1 justify-center btn btn-primary"
               value="Register"
             />
           </form>
@@ -121,25 +149,25 @@ const Register = (props: Props) => {
               </span>
             </div>
             <div className="grid grid-cols-2 gap-4">
-              <a href="#" className="py-3 btn btn-icon btn-google ">
-               <FaGoogle className="mr-1 text-2xl"/>
-                <span className="sr-only">Continue with</span> Google
+              <a href="#" className="py-3 hover:cursor-pointer  bg-[#e2edf2] text-[#25495c] hover:bg-[#25495c] hover:text-[#e2edf2]  btn btn-icon btn-google ">
+               <FaGoogle className="mr-1 text-2xl "/>
+                <span className="sr-only ">Continue with</span> Google
               </a>
-              <a href="#" className="py-3 btn btn-icon btn-dark">
+              {/* <a href="#" className="py-3 btn bg-[#e2edf2] text-[#25495c] hover:bg-[#25495c] hover:cursor-pointer hover:text-[#e2edf2]  btn-icon btn-dark">
                 <FaGithub className="mr-1 text-2xl"/>
                 <span className="sr-only">Continue with</span> Github
-              </a>
+              </a> */}
             </div>
           </div>
         </motion.div>
         <p className="mb-4  text-xs text-center text-gray-400">
-          <Link href='/login' className="text-purple-200 underline hover:text-white">
+          <Link href='/login' className="text-[#e2edf2] underline hover:text-white">
             Have An Account ? Login
           </Link>
           ·
           
           ·
-          <a href="#" className="text-purple-200 underline hover:text-white">
+          <a href="#" className="text-[#e2edf2] underline hover:text-white">
             Privacy & Terms
           </a>
         </p>
